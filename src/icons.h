89c40cc3c940e96f68121b4b3d1dad53dd3516ea
jeanleflambeur <catalin.vasile@gmail.com>
2019-04-24 19:04:42 +0200

    added icons

---
+++ src/icons.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <stdint.h>
+
+struct Image
+{
+  uint16_t  width;
+  uint16_t  height;
+  uint16_t  bytes_per_pixel; /* 2:RGB16, 3:RGB, 4:RGBA */ 
+  const char*  pixel_data;
+};
+
+extern Image k_imgOk;
+extern Image k_imgCancel;
+extern Image k_imgCardOk;
+extern Image k_imgCardError;
+extern Image k_img2Wire;
+extern Image k_img4Wire;
+
+
